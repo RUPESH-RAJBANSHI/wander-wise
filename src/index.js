31dd97c4f644bdaffe5ea827808a2a09f984afbc
RUPESH-RAJBANSHI <rajbanshi.Rupesh.555@gmail.com>
2026-01-20 18:30:23 +0545

    reip delete and trip details

--- src/index.js
+++ src/index.js
@@ -5,17 +5,26 @@ import connectDB from "./config/db.js";
 import HANDLERS from "./handlers/index.js";
 import errorMiddleware from "./middlewares/error.js";
 import { authMiddleware } from "./middlewares/auth.js";
+import cors from "cors";
 
 const SERVER = express();
 
 connectDB()
-  .then(() => {})
-  .catch(() => {})
-  .finally(() => {});
+.then(() => {})
+.catch(() => {})
+.finally(() => {});
 
 SERVER.get("/", (req, res,) => {
   res.send("Wander Wise");
 });
+
+SERVER.use(
+  cors({
+    origin: process.env.BASE_URL || "http://localhost:5173",
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 SERVER.use(express.json());
 SERVER.use(authMiddleware);
