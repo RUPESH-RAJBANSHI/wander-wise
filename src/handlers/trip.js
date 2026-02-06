955fcda6921445bbcb0312ce64f7b0997828709e
RUPESH-RAJBANSHI <rajbanshi.Rupesh.555@gmail.com>
2026-02-06 16:19:44 +0545

    usevalidator fix

--- src/handlers/trip.js
+++ src/handlers/trip.js
@@ -13,7 +13,7 @@ import {
   createTripValidator,
   updateTripValidator,
 } from "../validators/trip.js";
-import useValidator from "../middlewares/useValidator.js";
+import useValidator from "../middlewares/usevalidator.js";
 
 const TRIP_ROUTER = Router();
 
